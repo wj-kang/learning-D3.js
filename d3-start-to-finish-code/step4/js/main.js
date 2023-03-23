0718a8641ee603058939ea8b3e1e024b69814b00
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-22 20:57:14 -0500

    step4 - draw data

--- d3-start-to-finish-code/step4/js/main.js
+++ d3-start-to-finish-code/step4/js/main.js
@@ -1,21 +1,39 @@
 let data;
 
+const radiusScale = d3
+  .scaleSqrt() //
+  .domain([0, 100])
+  .range([0, 20]);
+
 function update() {
-    d3.select('#chart')
-        .selectAll('circle')
-        .data(data)
-        .join('circle')
-        .attr('cx', function(d, i) {
-            return i * 10;
-        })
-        .attr('cy', 100)
-        .attr('r', 2);
+  d3.select('#chart')
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('cx', function (d, i) {
+      return i * 10;
+    })
+    .attr('cy', 100)
+    .attr('r', (d) => radiusScale(d.renewable));
 }
 
 function dataIsReady(csv) {
-    data = csv;
-    update();
+  data = csv;
+  console.log(data);
+  update();
 }
 
-d3.csv('data/data.csv')
-    .then(dataIsReady);
+function csvRowConverter(row) {
+  const res = {};
+  Object.keys(row).forEach((key) => {
+    if (key === 'name' || key === 'id') {
+      res[key] = row[key];
+    } else {
+      res[key] = parseFloat(row[key]);
+    }
+  });
+
+  return res;
+}
+
+d3.csv('data/data.csv', csvRowConverter).then(dataIsReady);
