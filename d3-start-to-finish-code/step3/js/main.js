527e0b08d137ff7ec9c294f8eb31dcec38889985
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-22 20:36:39 -0500

    step3 - join data

--- d3-start-to-finish-code/step3/js/main.js
+++ d3-start-to-finish-code/step3/js/main.js
@@ -1,6 +1,22 @@
-function dataIsReady(csv) {
-    console.log(csv);
+let dataset;
+
+async function setData() {
+  dataset = await d3.csv('data/data.csv');
 }
 
-d3.csv('data/data.csv')
-    .then(dataIsReady);
+function update() {
+  d3.select('#chart') //
+    .selectAll('circle')
+    .data(dataset)
+    .join('circle')
+    .attr('cx', (d, i) => i * 10)
+    .attr('cy', 100)
+    .attr('r', 2);
+}
+
+async function init() {
+  await setData();
+  update();
+}
+
+init();
