b69f5fd2f7f4ae6e0bd803652424ecbcdfedee0d
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-23 20:46:29 -0500

    step8 - nested circles

--- d3-start-to-finish-code/step8/js/update.js
+++ d3-start-to-finish-code/step8/js/update.js
@@ -1,30 +1,31 @@
+function initGroupEl(g) {
+  g.classed('country', true);
+  g.append('circle').classed('renewable', true);
+  g.append('circle').classed('oilgascoal', true);
+  g.append('circle').classed('hydroelectric', true);
+  g.append('circle').classed('nuclear', true);
+  g.append('text').classed('label', true);
+}
+
 function updateGroup(d, i) {
-    let g = d3.select(this);
+  let g = d3.select(this);
 
-    if(g.selectAll('*').empty()) {
-        g.append('circle');
+  if (g.selectAll('*').empty()) {
+    initGroupEl(g);
+  }
 
-        g.append('text')
-            .classed('label', true);
-    }
+  g.attr('transform', 'translate(' + d.x + ',' + d.y + ')');
 
-    g.classed('country', true)
-        .attr('transform', 'translate(' + d.x + ',' + d.y + ')');
+  g.select('.renewable').attr('r', d.renewableRadius);
+  g.select('.oilgascoal').attr('r', d.oilGasCoalRadius);
+  g.select('.hydroelectric').attr('r', d.hydroelectricRadius);
+  g.select('.nuclear').attr('r', d.nuclearRadius);
 
-    g.select('circle')
-        .attr('r', d.radius);
-
-    g.select('.label')
-        .attr('y', d.labelOffset)
-        .text(d.labelText);
+  g.select('.label').attr('y', d.labelOffset).text(d.labelText);
 }
 
 function update() {
-    let layoutData = layout(data);
+  let layoutData = layout(data);
 
-    d3.select('#chart')
-        .selectAll('g')
-        .data(layoutData)
-        .join('g')
-        .each(updateGroup);
+  d3.select('#chart').selectAll('g').data(layoutData).join('g').each(updateGroup);
 }
