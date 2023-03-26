f6aba473356d80fc88280c157c1f7f0c6644dd90
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-26 16:47:31 -0500

    udemy/section7 - line chart

---
+++ udemy-learn-d3-data-visualization/07-line-chart/app.js
@@ -0,0 +1,63 @@
+async function draw() {
+  // Data
+  const dataset = await d3.csv('data.csv');
+
+  const parseDate = d3.timeParse('%Y-%m-%d');
+
+  const xAccessor = (d) => parseDate(d.date);
+  const yAccessor = (d) => parseInt(d.close);
+
+  // Dimensions
+  let dimensions = {
+    width: 1000,
+    height: 500,
+    margins: 50,
+  };
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
+
+  // Draw Image
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  const ctr = svg
+    .append('g') // <g>
+    .attr(
+      'transform',
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    );
+
+  // Scales
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.ctrHeight, 0])
+    .nice();
+
+  const xScale = d3
+    .scaleUtc() // work with UTC date
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.ctrWidth]);
+
+  // console.log(xScale(xAccessor(dataset[5])), dataset[5]);
+
+  const lineGenerator = d3
+    .line() //
+    .x((d) => xScale(xAccessor(d)))
+    .y((d) => yScale(yAccessor(d)));
+
+  // console.log(lineGenerator(dataset));
+  ctr
+    .append('path') //
+    .datum(dataset)
+    .attr('d', lineGenerator)
+    .attr('fill', 'none')
+    .attr('stroke', '#30475e')
+    .attr('stroke-width', 2);
+}
+
+draw();
