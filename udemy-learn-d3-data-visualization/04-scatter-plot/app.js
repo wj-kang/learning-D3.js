9957b5203c109b6a03eca8ef44221dfe05ad226b
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-25 23:12:33 -0500

    udemy/section4 - *add tooltip with voronoi diagram

--- udemy-learn-d3-data-visualization/04-scatter-plot/app.js
+++ udemy-learn-d3-data-visualization/04-scatter-plot/app.js
@@ -35,6 +35,8 @@ async function draw() {
     .append('g') //
     .attr('transform', `translate(${dimensions.margin.left}, ${dimensions.margin.top})`);
 
+  const tooltip = d3.select('#tooltip');
+
   // Scales
   const xScale = d3 //
     .scaleLinear()
@@ -98,6 +100,65 @@ async function draw() {
     .html('Temperature &deg; F')
     .style('transform', 'rotate(270deg)')
     .style('text-anchor', 'middle');
+
+  // Voronoi Diagram (Advanced)
+  const delaunay = d3.Delaunay.from(
+    dataset,
+    (d) => xScale(xAccesor(d)),
+    (d) => yScale(yAccesor(d))
+  );
+
+  const voronoi = delaunay.voronoi();
+  voronoi.xmax = dimensions.ctrWidth;
+  voronoi.ymax = dimensions.ctrHeight;
+
+  container
+    .append('g') //
+    .selectAll('path')
+    .data(dataset)
+    .join('path')
+    .attr('fill', 'transparent')
+    .attr('d', (d, i) => voronoi.renderCell(i))
+    .on('mouseenter', function (e, d) {
+      container
+        .append('circle') //
+        .classed('dot-hovered', true)
+        .attr('fill', '#120078')
+        .attr('r', 8)
+        .attr('cx', (_) => xScale(xAccesor(d)))
+        .attr('cy', (_) => yScale(yAccesor(d)))
+        .style('pointer-events', 'none');
+      // d3.select(this)
+      //   .attr('fill', '#120078') //
+      //   .attr('r', 8);
+
+      tooltip //
+        .style('display', 'block')
+        .style('top', yScale(yAccesor(d)) - 35 + 'px')
+        .style('left', xScale(xAccesor(d)) - 25 + 'px');
+
+      const formatter = d3.format('.2f');
+      const dateFormatter = d3.timeFormat('%B %-d, %Y');
+
+      tooltip
+        .select('.metric-humidity span') //
+        .text(formatter(xAccesor(d)));
+      tooltip
+        .select('.metric-temp span') //
+        .text(formatter(yAccesor(d)));
+      tooltip
+        .select('.metric-date') //
+        .text(dateFormatter(d.currently.time * 1000));
+    })
+    .on('mouseleave', function (e, d) {
+      container.select('.dot-hovered').remove();
+      // d3.select(this)
+      //   .attr('fill', 'brown') //
+      //   .attr('r', 5);
+
+      tooltip //
+        .style('display', 'none');
+    });
 }
 
 draw();
